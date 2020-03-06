9ed6b53854f79967146ecb5be0a1939f7960f714
Dzmitrysiniakou <jack.pott@mail.ru>
2020-03-06 13:37:10 +0300

    feat morse task done

--- src/index.js
+++ src/index.js
@@ -35,10 +35,26 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let letterArr = expr.split('');
+    let stringFinal = '';
+    while (letterArr.length > 0){
+        stringFinal += myFun(letterArr.splice(0,10));
+    } 
+    return stringFinal;
+}
+function myFun(dotArr) {
+    let letter = '';
+    for(let i=0; i<10; i += 2){
+        if (dotArr[i] == '0' && dotArr[i+1] == '0') letter += '';
+        if (dotArr[i] == '1' && dotArr[i+1] == '1') letter += '-';
+        if (dotArr[i] == '1' && dotArr[i+1] == '0') letter += '.';
+        if (dotArr[i] == '*') return ' ';
+    }
+    return MORSE_TABLE[letter];
 }
 
 module.exports = {
